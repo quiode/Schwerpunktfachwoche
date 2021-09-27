45264f1a520dcf12a588ba46238bbd1f19e84b19
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-09-27 15:01:17 +0200

    basic stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
-void setup() {
+#define TRIGGER_ABOVE 2
+#define ECHO_ABOVE 3
+
+#define ECHO_BELOW 4
+#define TRIGGER_BELOW 5
+
+void setup()
+{
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
+  delay(1000);
+  Serial.println("Start");
   // put your main code here, to run repeatedly:
 }
